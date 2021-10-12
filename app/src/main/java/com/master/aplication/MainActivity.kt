370589d062210187bee66d7ad8ce33fc79f73247
carlos ortega <carlosalbertoortegac@gmail.com>
2021-10-11 23:28:57 -0500

    se realiza enlace con la Screen pricipal

--- app/src/main/java/com/master/aplication/MainActivity.kt
+++ app/src/main/java/com/master/aplication/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.master.aplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startTimer()
     }
+    fun startTimer(){
+
+        object: CountDownTimer(3000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                var intent = Intent(applicationContext,PrincipalActivity::class.java).apply{}
+                startActivity(intent)
+            }
+
+        }.start()
+    }
+
 }
---
+++ app/src/main/java/com/master/aplication/PrincipalActivity.kt
@@ -0,0 +1,11 @@
+package com.master.aplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
