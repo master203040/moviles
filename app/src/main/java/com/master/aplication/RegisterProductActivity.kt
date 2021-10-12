9e8712dbdec32576049420b43d8c30767177caad
carlos ortega <carlosalbertoortegac@gmail.com>
2021-10-12 00:19:44 -0500

    Screen de register shop product

---
+++ app/src/main/java/com/master/aplication/RegisterProductActivity.kt
@@ -0,0 +1,11 @@
+package com.master.aplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_product)
+    }
+}
